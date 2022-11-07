95abb1e4df05d45f1bc4e4c048707dcc8003b7b3
Jeb <jebgriffin85@gmail.com>
2022-11-06 21:13:26 -0600

    first

---
+++ backend/db/migrations/20221006195814-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      score: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      currentLevel: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+      },
+      hints: {
+        type: Sequelize.INTEGER, 
+        defaultValue: 1
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ notes.md
@@ -0,0 +1,7 @@
+community levels - people can make their own
+score board
+each level completed- unlock a hint, hint saved on user. use one they -1 hint
+need current level 
+reset functionality to delete score, hints, and start over - no bc they could use all hints then restart?
+fun mode - randoms levels for unauth users or users who arent playing main game, hints disabled
+
